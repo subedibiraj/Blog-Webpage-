e7c3ab45512d05c9ca6cb4c52a1bdbc00a6fc070
subedibiraj <subedi.biraj655@gmail.com>
2024-06-06 18:12:14 +0545

    handled /post/id

--- server/routes/main.js
+++ server/routes/main.js
@@ -82,6 +82,29 @@ router.get('', async (req, res) => {
 
 
 
+//GET POST
+router.get('/post/:id', async (req, res) => {
+    try {
+      let slug = req.params.id;
+  
+      const data = await Post.findById({ _id: slug });
+  
+      const locals = {
+        title: data.title,
+        description: "Simple Blog created with NodeJs, Express & MongoDb.",
+      }
+  
+      res.render('post', { 
+        locals,
+        data,
+        currentRoute: `/post/${slug}`
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  
+  });
+
 router.get('/about',(req,res)=>{
     res.render('about');
 })
