86b5a7ed4b7b8c1027d11e05395850887ba07136
subedibiraj <subedi.biraj655@gmail.com>
2024-07-04 22:59:23 +0545

    solved search button problem

--- server/routes/main.js
+++ server/routes/main.js
@@ -111,7 +111,7 @@ router.get('/post/:id', async (req, res) => {
 router.post('/search', async (req, res) => {
     try {
       const locals = {
-        title: "Seach",
+        title: "Search",
         description: "Simple Blog created with NodeJs, Express & MongoDb."
       }
   
